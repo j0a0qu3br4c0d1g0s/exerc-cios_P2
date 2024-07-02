47619567a76553f46de42b92663bf7d0a7bad595
j0a0qu3br4c0d1g0s <166523332+j0a0qu3br4c0d1g0s@users.noreply.github.com>
2024-07-02 10:18:30 -0300

    Add files via upload

---
+++ Digitar numeros para serem somados e quando o usuário.py
@@ -0,0 +1,12 @@
+soma = 0
+
+while True:
+    
+        numero = int(input("Digite um número (ou 0 para sair): "))
+        if numero == 0:
+            break
+        soma += numero
+
+        print("Digite apenas números inteiros.")
+
+print(f"A soma de todos os números digitados é: {soma}")
