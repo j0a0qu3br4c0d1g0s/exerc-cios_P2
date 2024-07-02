453b8b0d4aed71711bc8816ad81124ed31eb753f
j0a0qu3br4c0d1g0s <166523332+j0a0qu3br4c0d1g0s@users.noreply.github.com>
2024-07-02 10:14:29 -0300

    Add files via upload

---
+++ vaga de emprego.py
@@ -0,0 +1,12 @@
+nome = "Yudi M. Alves"
+profissao = "Programador em Python"
+empresa = "Tech-X"
+localizacao = "Márica, RJ"
+descricao = "Procuramos um programador em Python para trabalhar em nossa empresa."
+
+print("Vaga de Emprego")
+print("Nome: ", nome)
+print("profissao: ", profissao)
+print("Empresa: ", empresa)
+print("Localização: ", localizacao)
+print("Descrição: ", descricao)
